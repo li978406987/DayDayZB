613b361e484c106208d3ae7d7caca6fd9f96d393
dllo <956149892@qq.com>
2016-11-10 08:56:14 +0800

    封装的Alamofire解析数据

--- DayDayZB/Classes/Home/Controller/LJ_RecommendViewController.swift
+++ DayDayZB/Classes/Home/Controller/LJ_RecommendViewController.swift
@@ -7,13 +7,16 @@
 //
 
 import UIKit
+import Alamofire
 
 fileprivate let kItemMagin : CGFloat = 10
 fileprivate let kItemW = (kScreenW - 3 * kItemMagin) / 2
-fileprivate let kItemH = kItemW * 3 / 4
+fileprivate let kNormalItemH = kItemW * 3 / 4
+fileprivate let kPrettyItemH = kItemW
 fileprivate let kHeaderViewH : CGFloat = 50
 
 fileprivate let kNormalCellID = "kNormalCellID"
+fileprivate let kPrettyCellID = "kPrettyCellID"
 fileprivate let kHeaderViewID = "HeaderViewID"
 
 class LJ_RecommendViewController: UIViewController {
@@ -22,7 +25,7 @@ class LJ_RecommendViewController: UIViewController {
        // 创建布局
         let layout = UICollectionViewFlowLayout()
         
-        layout.itemSize = CGSize(width : kItemW, height : kItemH)
+        layout.itemSize = CGSize(width : kItemW, height : kNormalItemH)
         
         layout.minimumLineSpacing = 0
         layout.minimumInteritemSpacing = kItemMagin
@@ -32,12 +35,16 @@ class LJ_RecommendViewController: UIViewController {
         // 创建UICollectionView
         let collectionView = UICollectionView(frame : self.view.bounds, collectionViewLayout : layout)
         
-        collectionView.backgroundColor = UIColor.blue
+        collectionView.backgroundColor = UIColor.white
         collectionView.dataSource = self
+        collectionView.delegate = self
         collectionView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: kNormalCellID)
+        
+        collectionView.register(UINib(nibName: "CollectionViewPrettyCell", bundle: nil), forCellWithReuseIdentifier: kPrettyCellID)
+        
+        collectionView.register(UINib(nibName: "CollectionViewNormalCell", bundle: nil), forCellWithReuseIdentifier: kNormalCellID)
 
-        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: kHeaderViewID)
+       collectionView.register(UINib(nibName: "CollectionHeaderView", bundle: nil), forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: kHeaderViewID)
        
         return collectionView
         
@@ -48,12 +55,12 @@ class LJ_RecommendViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-        
-        
         // 设置UI界面
         setupUI()
-        
+        NetWorkTools.requestData(type: .get, URLString: "http://httpbin.org/get") { (result) in
+            print(result)
+
+        }
     }
 
  
@@ -70,7 +77,7 @@ extension LJ_RecommendViewController {
 }
 
 // MRAK: - 遵守UICOllectionViewDataSource的数据源协议
-extension LJ_RecommendViewController : UICollectionViewDataSource {
+extension LJ_RecommendViewController : UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
    
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 12
@@ -82,19 +89,36 @@ extension LJ_RecommendViewController : UICollectionViewDataSource {
         }
         return 4
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         // 获取cell
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kNormalCellID, for: indexPath)
-        cell.backgroundColor = UIColor.red
+        var cell : UICollectionViewCell!
+        
+        // 取出cell
+        if indexPath.section == 1 {
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: kNormalCellID, for: indexPath)
+        } else {
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: kPrettyCellID, for: indexPath)
+        }
+        
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.section == 1 {
+            return CGSize(width: kItemW, height: kPrettyItemH)
+        }
+        return CGSize(width: kItemW, height: kNormalItemH)
+    }
+    
+    
+    
+    
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         // 取出sectionHeaderView
         let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: kHeaderViewID, for: indexPath)
-        
-        headerView.backgroundColor = UIColor.green
+
         
         return headerView
     }
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.2'
+
+target 'DayDayZB' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for DayDayZB
+pod 'Alamofire', '~> 4.0.0'
+end
